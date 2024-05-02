cdf006468c65fcc2b7b5ba14433aa4bcb1a0758d
Katerina <smilevskakaterina5@gmail.com>
2024-05-02 19:24:30 +0200

    Create Spring Boot application.

---
+++ speaker-diarization-backend/src/test/java/com/example/speakerdiarizationbackend/SpeakerDiarizationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speakerdiarizationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakerDiarizationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
